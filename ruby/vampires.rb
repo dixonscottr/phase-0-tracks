0cab6d129a829cc0b736c39aafc7121d26d50760
Scott Dixon <dixon.scottr@gmail.com>
2016-08-24 20:45:07 -0400

    Add loop to run program for multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,40 +1,50 @@
 puts "Welcome to Werewolf Inc."
 
-#ask for the name
-puts "What is your name?"
-name = gets.chomp
+#ask how many employees to process
+puts "How many employee would you like to process?"
+employee_num = gets.chomp.to_i
 
-#ask for age and year of birth
-puts "How old are you?"
-age = gets.chomp.to_i
-puts "And what year were you born?"
-year_born = gets.chomp.to_i
+#run loop for each employee
+for i in 1..employee_num
 
-#ask if they want garlic bread
-puts "Would you like garlic bread in the company cafeteria? y/n"
-garlic_bread = gets.chomp
+  #ask for the name
+  puts "What is your name?"
+  name = gets.chomp
 
-#ask if they'd like health insurance
-puts "Would you like to enroll in our health insurance? y/n"
-health_insurance = gets.chomp
+  #ask for age and year of birth
+  puts "How old are you?"
+  age = gets.chomp.to_i
+  puts "And what year were you born?"
+  year_born = gets.chomp.to_i
 
-vampire_status = "Results inconclusive"
+  #ask if they want garlic bread
+  puts "Would you like garlic bread in the company cafeteria? y/n"
+  garlic_bread = gets.chomp
 
-if (age == (2016-year_born)) && (garlic_bread == "y" || health_insurance == "y")
-  vampire_status = "Probably not a vampire"
+  #ask if they'd like health insurance
+  puts "Would you like to enroll in our health insurance? y/n"
+  health_insurance = gets.chomp
+
+  vampire_status = "Results inconclusive"
+
+  if (age == (2016-year_born)) && (garlic_bread == "y" || health_insurance == "y")
+    vampire_status = "Probably not a vampire"
+  end
+
+  if !(age == (2016-year_born)) && (garlic_bread == "n" || health_insurance == "n")
+    vampire_status =  "Probably a vampire"
+  end
+
+  if !(age == (2016-year_born)) && garlic_bread == "n" && health_insurance == "n"
+    vampire_status =  "Almost certainly a vampire"
+  end
+
+  if name == "Drake Cula" || name == "Tu Fang"
+    vampire_status = "Definitely a vampire"
+  end
+
+  puts vampire_status
+  puts " "
+
 end
-
-if !(age == (2016-year_born)) && (garlic_bread == "n" || health_insurance == "n")
-  vampire_status =  "Probably a vampire"
-end
-
-if !(age == (2016-year_born)) && garlic_bread == "n" && health_insurance == "n"
-  vampire_status =  "Almost certainly a vampire"
-end
-
-if name == "Drake Cula" || name == "Tu Fang"
-  vampire_status = "Definitely a vampire"
-end
-
-puts vampire_status
 
