a246de8f0cf04a70a3cd77a22e1e577914460792
Scott Dixon <dixon.scottr@gmail.com>
2016-08-30 10:18:00 -0400

    Add pseudocode to hashes.rb

---
+++ ruby/hashes.rb
@@ -0,0 +1,23 @@
+#Create an empty hash
+
+#Welcome user and explain program
+
+#Ask user for input regarding client's name, age, hometown, current city, number of children, decor theme
+#color preference, whether they have pets, number of rooms they want redecorated, budget
+
+#store the above data as a key value pair in the hash
+
+#print the completed hash to the screen
+
+#Ask the user if they would like to update any keys
+
+#If user chooses a key, ask them for the input and update with the new value
+
+#If the user chooses "none", do nothing
+
+#print the latest version of the hash
+
+
+
+
+
