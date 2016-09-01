1d418d21fb024bc4f3b54692d235b395964ef2d7
Scott Dixon <dixon.scottr@gmail.com>
2016-08-31 22:43:26 -0400

    Add vowel_shift method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -11,10 +11,31 @@ def to_lowercase(full_name)
   lowercase_name = full_name.downcase
 end
 
-p to_lowercase("John Smith")
-puts to_lowercase("John Smith") == "john smith"
+# p to_lowercase("John Smith")
+# puts to_lowercase("John Smith") == "john smith"
 
 #Go through the word and change all vowels (aeiou) to the next vowel
+
+def vowel_shift(full_name)
+  vowels = "aeiou"
+  vowel_shifted_name = []
+  full_name.chars do |letter|
+    if vowels.include?(letter)
+      vowel_idx = vowels.index(letter)
+      if vowel_idx < 4
+        vowel_shifted_name << vowels[vowel_idx + 1]
+      else
+        vowel_shifted_name << "a"
+      end
+    else
+      vowel_shifted_name << letter
+    end
+  end
+  vowel_shifted_name.join("")
+end
+
+p vowel_shift("John Smith")
+puts vowel_shift("John Smith") == "Juhn Smoth"
 
 #Go through the word and change all consonants (bcdfghjklmnpqrstvwxyz) to the next one
 
