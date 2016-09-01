421ed57fe5119d016d4d115941b99b47215b693f
Scott Dixon <dixon.scottr@gmail.com>
2016-08-31 21:59:13 -0400

    Add pseudocode to alias_manager.rb

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,11 @@
+#Swap first and last name
+
+#Change all letters to lowercase
+
+#Go through the word and change all vowels (aeiou) to the next vowel
+
+#Go through the word and change all consonants (bcdfghjklmnpqrstvwxyz) to the next one
+
+#Capitalize the first letters of both names
+
+#Return the fake name
