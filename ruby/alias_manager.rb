be7b788bc2d2a54e8fa5df3b09a70d1baa83b765
Scott Dixon <dixon.scottr@gmail.com>
2016-08-31 22:10:45 -0400

    Add to_lowercase method to alias_manager.rb

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,10 +3,16 @@ def swap_name(full_name)
   new_name = full_name.split(" ").reverse.join(" ")
 end
 
-p swap_name("John Smith")
-puts swap_name("John Smith") == "Smith John"
+# p swap_name("John Smith")
+# puts swap_name("John Smith") == "Smith John"
 
 #Change all letters to lowercase
+def to_lowercase(full_name)
+  lowercase_name = full_name.downcase
+end
+
+p to_lowercase("John Smith")
+puts to_lowercase("John Smith") == "john smith"
 
 #Go through the word and change all vowels (aeiou) to the next vowel
 
