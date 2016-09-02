237dc3564b3ead8c99870e0dc0e9009febe826c1
Scott Dixon <dixon.scottr@gmail.com>
2016-09-02 11:20:47 -0400

    Create get_fib_number method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -12,3 +12,20 @@ end
 
 #release 1
 
+def get_fib_number(fib_nums_wanted)
+  fib_array = [0, 1]
+  index = 0
+  
+  while index < (fib_nums_wanted - 2)
+    first_num = fib_array[index]
+    second_num = fib_array[index + 1]
+    fib_num = first_num + second_num
+    fib_array << fib_num
+    index += 1
+  end
+  fib_array
+end
+
+puts get_fib_number(6) ==  [0,1,1,2,3,5]
+puts get_fib_number(100).last == 218922995834555169026
+p get_fib_number(5)
