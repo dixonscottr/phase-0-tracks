d3fe57dadbf1ec265d6ea2c7c794af346007e050
Scott Dixon <dixon.scottr@gmail.com>
2016-09-07 11:07:51 -0400

    Begin program

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,14 +1,14 @@
 class Santa
 
-  attr_reader :age
+  attr_reader :age, :ethnicity
   attr_accessor :gender
 
-  def initialize(gender, ethnicity)
+  def initialize(gender, ethnicity, age=0)
     puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0
+    @age = age
   end
   
   def speak
@@ -46,33 +46,50 @@ end
 # st_nick.speak
 # st_nick.eat_milk_and_cookies("gingersnap")
 
-santas = []
+# santas = []
 
 genders = ["male", "female", "gender fluid", "trans*", "agender", "other", "prefer not to say"]
 
 ethnicities = ["black", "white", "Asian", "Native American", "Latino", "Arab", "Persian", "Aboriginal Australian", "other"]
 
+# 10.times do
+#   gender = genders.shuffle.first
+#   ethnicity = ethnicities.shuffle.first
+#   santas << Santa.new(gender, ethnicity)
+# end
+
+# p santas[8]
+
+# p santas[8].celebrate_birthday
+# p santas[8].celebrate_birthday
+# p santas[8].celebrate_birthday
+
+# p santas[8]
+# p santas[8].get_mad_at("Dasher")
+
+# p santas[8]
+# p santas[8].gender = "female"
+
+# p santas[8].age
+# p santas[8].gender
+
+santa_num = 1
+
 10.times do
-  gender = genders.shuffle.first
-  ethnicity = ethnicities.shuffle.first
-  santas << Santa.new(gender, ethnicity)
+  gender = genders.sample
+  ethnicity = ethnicities.sample
+  age = rand(0..140)
+  puts ""
+  puts "Creating Santa no. #{santa_num}"
+  santa = Santa.new(gender, ethnicity, age)
+
+  p santa.age
+  p santa.gender
+  p santa.ethnicity
+
+
+  santa_num += 1
 end
-
-p santas[8]
-
-p santas[8].celebrate_birthday
-p santas[8].celebrate_birthday
-p santas[8].celebrate_birthday
-
-p santas[8]
-p santas[8].get_mad_at("Dasher")
-
-p santas[8]
-p santas[8].gender = "female"
-
-p santas[8].age
-p santas[8].gender
-
 
 
 
