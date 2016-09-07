67ec07c362f85fb8f42bc7692cb53f5f1b6cb80f
Scott Dixon <dixon.scottr@gmail.com>
2016-09-06 20:27:07 -0400

    Add Robot class and methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,9 @@
 class Puppy
 
+  def initialize
+    puts "initializing new puppy instance"
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -37,15 +41,49 @@ class Puppy
 
 end
 
-
 spot = Puppy.new
 
 spot.speak(3)
-
 spot.bark(3)
-
 spot.roll_over
-
 spot.dog_years(3)
-
 spot.make_circles(3)
+
+
+#new Class
+
+class Robot
+
+  def initialize
+    puts "WELCOME TO ROBOTLAND, NEW ROBOT!"
+  end
+
+  def robot_dance
+    puts "I'm dancing like a pro"
+  end
+
+  def exterminate(target)
+    puts "EXTERMINATE ALL #{target}"
+  end
+
+end
+
+new_robots = []
+
+50.times do
+new_robots << Robot.new 
+end
+
+
+# p new_robots
+
+new_robots.each do |robot|
+  robot.robot_dance
+  robot.exterminate("HUMANS")
+end
+
+
+
+
+
+
