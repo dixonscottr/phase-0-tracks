e217f3276d1ccb47419c9e2c6f595ea51961ddf6
Scott Dixon <dixon.scottr@gmail.com>
2016-09-07 23:09:19 -0400

    Add two classes with Shout as mixin and driver code to test

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -23,3 +23,19 @@ module Shout
   end
 
 end
+
+class Librarian
+  include Shout
+end
+
+class TrainConductor
+  include Shout
+end
+
+lori_beth = Librarian.new
+puts lori_beth.yell_angrily("Be Quiet! This is a library!")
+puts lori_beth.yell_happily("Children are reading quietly!")
+
+teri = TrainConductor.new
+puts teri.yell_angrily("Stand back and let the doors close!")
+puts teri.yell_happily("Thank you for riding with the MTA!")
