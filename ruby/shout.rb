ed273d8c40dff7d2367d6d7a87e539e550652001
Scott Dixon <dixon.scottr@gmail.com>
2016-09-07 23:01:41 -0400

    Change Shout from standalone to mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,25 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     " (＠＾－＾)   " + words + "   (＾－＾@)"
+#   end
+
+# end
+
+# p Shout.yell_angrily("I am so mad!")
+# p Shout.yell_happily("I am so happy!")
+
 module Shout
-  def self.yell_angrily(words)
+
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
     " (＠＾－＾)   " + words + "   (＾－＾@)"
   end
 
 end
-
-p Shout.yell_angrily("I am so mad!")
-p Shout.yell_happily("I am so happy!")
