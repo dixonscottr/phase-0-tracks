68843529dfa40b36f6c6dafab311ec652873790c
Scott Dixon <dixon.scottr@gmail.com>
2016-09-07 22:52:50 -0400

    Add yell_happily method and driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -2,4 +2,12 @@ module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end
+
+  def self.yell_happily(words)
+    " (＠＾－＾)   " + words + "   (＾－＾@)"
+  end
+
 end
+
+p Shout.yell_angrily("I am so mad!")
+p Shout.yell_happily("I am so happy!")
