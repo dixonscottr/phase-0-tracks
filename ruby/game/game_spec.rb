e5f2600c85a585ca6c132aef372f0fd93666cf48
Scott Dixon <dixon.scottr@gmail.com>
2016-09-10 09:04:30 -0400

    Add rspec tests for guess_letter method

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,36 +1,61 @@
 require_relative "game"
 
-describe MatchingGame do
-  let(:game) {MatchingGame.new}
+  describe MatchingGame do
+    let(:game) {MatchingGame.new}
 
-it "replaces an underscore with the correct letter" do
-  game.mystery_word = "hello"
-  game.guessed_word = "_____"
-  expect(game.replace_blank("h")).to eq("h____")
-end
-
-it "replaces two or more underscores with the correct letters" do
-  game.mystery_word = "hello"
-  game.guessed_word = "_____"
-  expect(game.replace_blank("l")).to eq("__ll_")
-end
-
-it "replaces all letters of a word with underscores" do
-  expect(game.create_mystery_word("hello")).to eq("_____")
-end
-
-it "replaces only the letters in a phrase with underscores" do
-  expect(game.create_mystery_word("hello world")).to eq("_____ _____")
+  it "replaces an underscore with the correct letter" do
+    game.mystery_word = "hello"
+    game.guessed_word = "_____"
+    expect(game.replace_blank("h")).to eq("h____")
   end
 
-it "makes the number of guesses equal to the number of letters in a word" do
-  expect(game.create_mystery_word("hello"))
-  expect(game).to have_attributes guess_count: 5
-end
+  it "replaces two or more underscores with the correct letters" do
+    game.mystery_word = "hello"
+    game.guessed_word = "_____"
+    expect(game.replace_blank("l")).to eq("__ll_")
+  end
 
-it "makes the number of guesses equal to the number of letters in a phrase" do
-  expect(game.create_mystery_word("hello world"))
-  expect(game).to have_attributes guess_count: 10
-end
+  it "replaces all letters of a word with underscores" do
+    expect(game.create_mystery_word("hello")).to eq("_____")
+  end
+
+  it "replaces only the letters in a phrase with underscores" do
+    expect(game.create_mystery_word("hello world")).to eq("_____ _____")
+    end
+
+  it "makes the number of guesses equal to the number of letters in a word" do
+    game.create_mystery_word("hello")
+    expect(game).to have_attributes guess_count: 5
+  end
+
+  it "makes the number of guesses equal to the number of letters in a phrase" do
+    game.create_mystery_word("hello world")
+    expect(game).to have_attributes guess_count: 10
+  end
+
+  it "guesses a letter and updates the guessed word if correct" do
+    game.create_mystery_word("hello")
+    expect(game.guess_letter("h")).to eq ("h")
+  end
+
+  it "guesses a letter correctly and decreases the guess count by 1" do
+    game.create_mystery_word("hello")
+    game.guess_letter("h")
+    expect(game).to have_attributes guess_count: 4
+  end
+
+  it "guesses a letter incorrectly and decreases the guess count by 1" do
+    game.create_mystery_word("hello")
+    game.guess_letter("x")
+    expect(game).to have_attributes guess_count: 4
+  end
+
+  it "guesses a previously guessed letter and keeps the same guess count" do
+    game.create_mystery_word("hello")
+    game.guess_letter("h")
+    game.guess_letter("h")
+    expect(game).to have_attributes guess_count: 4
+  end
+
 
 end
