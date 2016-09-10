b02c24c87dc03a09e973b55b10e891abf70afcce
Scott Dixon <dixon.scottr@gmail.com>
2016-09-09 23:34:46 -0400

    Add rspec tests to test create_mystery_word method

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -15,4 +15,22 @@ it "replaces two or more underscores with the correct letters" do
   expect(game.replace_blank("l")).to eq("__ll_")
 end
 
+it "replaces all letters of a word with underscores" do
+  expect(game.create_mystery_word("hello")).to eq("_____")
+end
+
+it "replaces only the letters in a phrase with underscores" do
+  expect(game.create_mystery_word("hello world")).to eq("_____ _____")
+  end
+
+it "makes the number of guesses equal to the number of letters in a word" do
+  expect(game.create_mystery_word("hello"))
+  expect(game).to have_attributes guess_count: 5
+end
+
+it "makes the number of guesses equal to the number of letters in a phrase" do
+  expect(game.create_mystery_word("hello world"))
+  expect(game).to have_attributes guess_count: 10
+end
+
 end
