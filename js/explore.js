81b013fea1584e239cf4f36f4a73d05a990c1f1d
Scott Dixon <dixon.scottr@gmail.com>
2016-09-14 09:08:44 -0400

    Add reverseString function with driver code

--- js/explore.js
+++ js/explore.js
@@ -6,3 +6,21 @@
 //   Move one character towards the beginning of the word and repeat
 // Return the formerly empty word called 'reversed word'
 
+function reverseString(str) {
+  var reversedWord = "";
+
+  for (i = (str.length - 1); i >= 0; i--) {
+    reversedWord = reversedWord + str[i].toLowerCase();
+  }
+  // capitalizes the first letter of the word
+  return reversedWord.charAt(0).toUpperCase() + reversedWord.slice(1);
+}
+
+var checkWord = reverseString("Goblin");
+console.log(checkWord == "Nilbog");
+
+if (checkWord == "Nilbog") {
+  console.log(checkWord + " is goblin spelled backwards.");
+} else {
+  console.log(checkWord + " means nothing to me.");
+}
