bcce2a0e100ea3b0109d09fc6417d79f68198e82
Scott Dixon <dixon.scottr@gmail.com>
2016-09-14 08:41:45 -0400

    Create explore.js and add pseudocode for reverse function

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// Pseudocode
+//
+// Create an empty word called 'reversed word'
+// For each character from the existing word:
+//   Starting from the last character, take it and add it to the empty word
+//   Move one character towards the beginning of the word and repeat
+// Return the formerly empty word called 'reversed word'
+
