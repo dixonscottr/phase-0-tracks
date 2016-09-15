212aef6416f31ef4f613616e6758c49abeafb314
Scott Dixon <dixon.scottr@gmail.com>
2016-09-15 09:36:46 -0400

    Add findLongestWord function with driver code

--- js/algos.js
+++ js/algos.js
@@ -6,8 +6,7 @@
 // Sort the items by length:
 //     Start at the first item and second item
 //     Compare the number of letters
-//     Create a value to track whether or not words have been switched and set it to false
-//     Create a counter to track the first word to compare
+//     Create a value to track whether or not words have been switched and set it to true
 //     IF the first word has more letters than the second:
 //       Switch the first and second words around
 //       Change the words switched value to true
@@ -16,5 +15,29 @@
 //   Repeat until no words need to be switched
 // Return the last item in the sorted list
 
+function findLongestWord(array) {
+  
+  var switched = true;
 
+  //continue checking until no numbers have been switched
+  while (switched === true) {
+    //change switched to false in case no numbers need to be switched
+    switched = false;
+    for (i = 0; i < (array.length - 1); i++) {
+      if (array[i].length > array[i + 1].length) {
+        var temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        // console.log(array);
+        // change switched back to true if numbers have been switched
+        switched = true;
+      }
+    }
+  }
 
+  return array[array.length - 1];
+
+}
+
+var words = ["abracadabra", "noshebetterdont", "greatness", "magical", "boat", "garden", "beach", "a", "marlin", "log", "beaver"];
+console.log(findLongestWord(words));
