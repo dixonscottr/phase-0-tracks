357a27901f3c39ad038a2e93cd2acdf0d03d39bb
Scott Dixon <dixon.scottr@gmail.com>
2016-09-25 19:39:01 -0400

    Put pseudocode in another file

--- databases/program/diane.rb
+++ databases/program/diane.rb
@@ -1,33 +1,3 @@
-  # pseudocode
-  #
-  # in d.i.a.n.e. database, create categories table with one list: name
-  #
-  # in d.i.a.n.e. database, create ideas table if note already created
-  # ideas table will have 5 lists: category, description, time required, done status, cost (as 0 to 5 $s)
-  #
-  # in d.i.a.n.e. database, create logged events table if not already created
-  # logged events table will have 5 lists: date, category, description, time taken, cost
-  #
-  # Accept as input: date, category, description, time and cost in list form
-  # Store as a new row in the logged events table
-  # IF that event's description isn't in the ideas table:
-  #  Store the category, description, time taken (as time required), cost and done status as true
-  # IF that event's description is in the ideas table:
-  #  UPDATE the done status to true
-  #
-  # Accept as input: category, description, time, cost
-  # Store as a new row in the ideas tables with done status as false
-  #
-  # Accept as input: category name
-  # Store as a new row in the categories table
-  # 
-  # Accept as (all optional) inputs: category, time, cost
-  # Print out the ideas table according set to the conditions
-  # IF no input:
-  #   Print out the full ideas table
-  # 
-  # Print out the logged events table neatly
-
 require 'sqlite3'
 
 db = SQLite3::Database.new("diane.db")
