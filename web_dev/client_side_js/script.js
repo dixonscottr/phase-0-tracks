026b4aecb2452ff84210b8af0021195a17effcc2
Scott Dixon <dixon.scottr@gmail.com>
2016-09-27 10:33:28 -0400

    Add two changes to the dom using JS

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,17 @@
+function turnToBlue(event) {
+  console.log("Turning the word to blue");
+  console.log(event);
+  event.target.style.color = "blue";
+}
+
+var text = document.getElementById("wonderful");
+text.addEventListener("click", turnToBlue);
+
+function surroundInRed(event) {
+  console.log("Surrounding it in Red");
+  console.log(event);
+  event.target.style.border = "6px solid red";
+}
+
+var title = document.getElementById("hello");
+title.addEventListener("click", surroundInRed);
