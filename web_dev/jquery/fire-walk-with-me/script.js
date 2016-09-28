d52ed61c0ef4802e5aadced974ffffbd159159d1
Scott Dixon <dixon.scottr@gmail.com>
2016-09-28 11:00:51 -0400

    Add simple jQuery animation

---
+++ web_dev/jquery/fire-walk-with-me/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+  var bigText = function(elem) {
+    elem.css('color', 'black');
+    elem.animate({fontSize: "25px"}, 1000)
+    elem.css('opacity', "1");
+  }
+
+  var fadeSlightly = function(elem) {
+    elem.css("opacity", "0.5");
+  }
+
+  $("#start-button").click(function(){
+    $("#poem").toggle(100);
+  });
+
+  $("#line-one").mouseenter(function(){
+    bigText($(this));
+  });
+
+ $("#line-one").mouseleave(function(){
+    fadeSlightly($(this));
+  });
+
+  $("#line-two").mouseenter(function(){
+    bigText($(this));
+  });
+
+  $("#line-two").mouseleave(function(){
+    fadeSlightly($(this));
+  });
+
+  $("#line-three").mouseenter(function(){
+    bigText($(this));
+  });
+
+  $("#line-three").mouseleave(function(){
+    fadeSlightly($(this));
+  });
+
+  $("#line-four").mouseenter(function(){
+    bigText($(this));
+  });
+
+  $("#line-four").mouseleave(function(){
+    fadeSlightly($(this));
+  });
+
+});
